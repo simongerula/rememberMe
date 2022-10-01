ef9ff43bea4c7c59471f675c9b1f0fd4e62696bd
Simon <simon@Simons-MacBook-Pro.local>
2022-10-01 18:09:29 +1300

    debugging req.query

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -7,6 +7,10 @@ router.get('/webhook', (req,res) =>{
     const mode = req.query['hub.mode']
     const token = req.query['hub.verify_token']
 
+    console.log(req.query)
+    console.log('////')
+    console.log(req.query['hub.mode'])
+    
     if (mode === 'suscribe' && token === VERIFY_TOKEN){
         res.send("Welcome to Recuerdame API")
     }
